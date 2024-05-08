7b2ae28059daffbcd5748e03093a6032a05cdd2e
Diana Colonescu <colonescudiana@yahoo.com>
2024-05-09 00:44:06 +0300

    Added health parameters computation

--- RaspPi_LocalServer.py
+++ RaspPi_LocalServer.py
@@ -5,7 +5,8 @@ import time
 import logging
 import firebase_admin
 from firebase_admin import credentials, firestore
-from datetime import datetime, timedelta
+import numpy as np
+from scipy.signal import find_peaks
 
 
 # Flask setup
@@ -23,7 +24,7 @@ db = firestore.client()
 device_user_table = {}
 
 def update_device_user_table():
-    #Update the local table with deviceId to userId mappings from Firestore.
+    # Update the local table with deviceId to userId mappings from Firestore.
     
     users_ref = db.collection('UserAuthList')
     docs = users_ref.stream()
@@ -38,25 +39,76 @@ def get_user_id_by_device_id(device_id):
     if device_id not in device_user_table:
         # Update the cache if deviceId is not found, indicating a potential new registration
         update_device_user_table()
-    print("Current Device-User Mapping:")
-    for device, user in device_user_table.items():
-        print(f"Device ID: {device} -> User ID: {user}")
+
     return device_user_table.get(device_id)
     
     
-def convert_elapsed_time_to_exact_time(initial_timestamp, elapsed_time_values):
-    #Convert initial timestamp to a datetime object
-    initial_timestamp = datetime.strptime(initial_timestamp, '%Y-%m-%d %H:%M:%S')
+def generate_dictionary_ecg_value_timestamp(initial_timestamp, ecg_values, sampling_interval):
     
-    #Initialize array for exact time values
-    time_values = []
-    
-    for value in elapsed_time_values:
-        #Compute the time by adding ellapsed milliseconds to the initial time
-        reading_time = initial_timestamp + timedelta(milliseconds=value)
-        time_values.append(reading_time)
+    # Initialize dictionary of the form {ecg_value, timestamp}
+    ecg_data = [ {"ecg_value": value, "timestamp": int(initial_timestamp) + i* sampling_interval} for i,value in enumerate(ecg_values)]
         
-    return time_values
+    return ecg_data
+    
+def compute_health_parameters(ecg_values):
+    # Detect R peaks
+    # The minimum distance for the peak computation is 50 values, which means once every 50 * 10 ms = 500 ms 
+    peaks, _ = find_peaks(ecg_values, height=None, distance = 50)
+    
+    # Compute RR intervals in ms (multiply by 10)
+    rr_intervals = np.diff(peaks) * 10
+    
+    #Compute the average value of rr intervals in ms
+    average_rr_interval = np.mean(rr_intervals)
+    
+    # Compute BPM
+    bpm = int (60000/average_rr_interval)
+    
+    # Compute SDNN (standard deviation of RR intervals)
+    sdnn = np.std(rr_intervals)
+    
+    # Compute RMSSD (root mean square of successive fifferences between consecutive heartbeats)
+    # Square the differences between peaks
+    squared_diffs = rr_intervals
+    
+    # Compute the mean
+    mean_squared_diffs = np.mean(squared_diffs)
+    
+    # Compute square root of the mean
+    rmssd = np.sqrt(mean_squared_diffs)
+    
+    return rr_intervals,bpm,sdnn,rmssd
+    
+    
+def upload_data(initial_timestamp,eq_id,ecg_values):
+    # Use cache to get userId
+    user_id = get_user_id_by_device_id(eq_id)
+        
+    if not user_id:
+        print(f"Eq id {eq_id} is not recogognized!")
+    else:
+
+        # Construct dictionary ecg_value , timestamp
+        ecg_data = generate_dictionary_ecg_value_timestamp(initial_timestamp, ecg_values, sampling_interval=10)
+        
+        # Get health parameters
+        rr_intervals, bpm, sdnn, rmssd = compute_health_parameters(ecg_values)
+
+
+        # Firestore data upload
+        try:
+            doc_ref = db.collection('ecg_data').document(user_id).collection('readings').document(initial_timestamp)
+            doc_ref.set({
+            "ecg_data": ecg_data,
+            "rr_intervals": rr_intervals.tolist(),
+            "bpm": bpm,
+            "sdnn": sdnn,
+            "rmssd": rmssd
+            })
+            print("Data uploaded successfully.")
+        except Exception as e:
+            print(f"Failed to upload data: {e}")
+            
     
 
 @app.route('/post-data', methods=['POST'])
@@ -66,24 +118,8 @@ def post_data():
         initial_timestamp = data.get('initialTimestamp')  
         eq_id = data.get('eqID')
         ecg_values = data.get('ecgValues', [])
-        elapsed_time_values = data.get('elapsedTimeValues',[])
-
-        # Use cache to get userId
-        user_id = get_user_id_by_device_id(eq_id)
         
-        if not user_id:
-            return jsonify({"message": "Device ID not associated with any user"}), 400
-
-        # Convert string timestamp to datetime object and calculate real timestamps
-        real_timestamps = convert_elapsed_time_to_exact_time(initial_timestamp, elapsed_time_values)
-        
-        # Convert initial time to document format for Firestore document name
-        document_timestamp = datetime.strptime(initial_timestamp, '%Y-%m-%d %H:%M:%S').strftime('%Y%m%d%H%M%S')
-        readings_data = [{'ecgValue': ecg_value, 'real_timestamp': timestamp.strftime('%H:%M:%S.%f')[:-3]} for ecg_value, timestamp in zip(ecg_values, real_timestamps)]
-
-        # Firestore data upload
-        doc_ref = db.collection('ecg_data').document(user_id).collection('readings').document(document_timestamp)
-        doc_ref.set({'readings': readings_data})
+        upload_data(initial_timestamp,eq_id,ecg_values)
         
         return jsonify({"message": "Data received successfully"}), 200
     except Exception as e:
