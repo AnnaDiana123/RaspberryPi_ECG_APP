29800513615703c2803e2350d16b17499c3f441a
AnnaDiana123 <colonescudiana@yahoo.com>
2024-06-06 01:14:23 +0300

    Added file error functionality

--- RaspPi_LocalServer.py
+++ RaspPi_LocalServer.py
@@ -2,19 +2,19 @@ from flask import Flask, request, jsonify
 import threading
 import collections
 import time
-import logging
 import firebase_admin
 from firebase_admin import credentials, firestore
 import numpy as np
 from scipy.signal import find_peaks
-import threading
+import json
+import os
 
 
 # Flask setup
 app = Flask(__name__)
 
 # Path to Firebase Admin SDK private Key
-cred = credentials.Certificate('ecgapp-7d874-firebase-adminsdk-obhri-ee5abe4640.json')
+cred = credentials.Certificate("ecgapp-7d874-firebase-adminsdk-obhri-ee5abe4640.json")
 firebase_admin.initialize_app(cred)
 
 # Get a Firestore Client
@@ -26,18 +26,41 @@ device_user_table = {}
 
 # Initialize a lock, returns an instance of the Lock class
 device_user_table_lock = threading.Lock();
+file_lock = threading.Lock();
 
+ERROR_FILE = "local_storage_errors.json"
+
+def write_to_error_file(error_message, eq_id, initial_timestamp, ecg_values):
+    # Write in a local file the data that is not transmitted to cloud due to an error
+    
+    json_entry= {
+        "error_message": error_message,
+        "initial_timestamp": initial_timestamp,
+        "eq_id": eq_id,
+        "ecg_values" : ecg_values
+    }
+    
+    # Use lock to write safely
+    with file_lock:
+        # Open file in append mode
+        file = open(ERROR_FILE, "a")
+        try:
+            file.write(json.dumps(json_entry) + "\n") # Write to file
+        finally:
+            file.close() # Close the file
+    
+        
 def update_device_user_table():
     # Update the local table with deviceId to userId mappings from Firestore.
     
     # Reference to UserAuthList collection
-    users_ref = db.collection('UserAuthList')
+    users_ref = db.collection("UserAuthList")
     # Get all documents in the collection
     docs = users_ref.stream()
     
     #Modify the global variable device_user_table
     global device_user_table
-    new_table = {doc.to_dict().get('DeviceId'): doc.id for doc in docs} # Create new mapping table deviceId : userId
+    new_table = {doc.to_dict().get("DeviceId"): doc.id for doc in docs} # Create new mapping table deviceId : userId
     
     # Use the lock so that the table update is thread-safe
     with device_user_table_lock:
@@ -45,12 +68,10 @@ def update_device_user_table():
     
 def get_user_id_by_device_id(device_id):
     # Retrieve userId from the local cache using deviceId. Update cache if necessary.
-    
-    # Ensure thread-safe read of device_user tableth lock
-    with device_user_table_lock:
-        if device_id not in device_user_table:
-            # Update the cache if deviceId is not found, indicating a potential new registration
-            update_device_user_table()
+
+    if device_id not in device_user_table:
+        # Update the cache if deviceId is not found, indicating a potential new registration
+        update_device_user_table()
 
     return device_user_table.get(device_id) # Return userId or None
     
@@ -94,13 +115,19 @@ def compute_health_parameters(ecg_values):
     
     
 def upload_data(initial_timestamp,eq_id,ecg_values):
-    # Up[load ecg data and computed health parameters to Firestore
-    
+    # Upload ecg data and computed health parameters to Firestore
+
     # Use cache to get userId
     user_id = get_user_id_by_device_id(eq_id)
-        
+
     if not user_id:
-        print(f"Eq id {eq_id} is not recogognized!")
+        error_message = f"Eq id {eq_id} is not recogognized!"
+        print(error_message)
+        write_to_error_file(error_message, initial_timestamp, eq_id, ecg_values)
+    elif -1 in ecg_values:
+        error_message = "Lead off detected! Data will not be forwarded to cloud"
+        print(error_message)
+        write_to_error_file(error_message, initial_timestamp, eq_id, ecg_values)
     else:
 
         # Construct dictionary ecg_value , timestamp
@@ -111,7 +138,7 @@ def upload_data(initial_timestamp,eq_id,ecg_values):
 
         # Firestore data upload
         try:
-            doc_ref = db.collection('ecg_data').document(user_id).collection('readings').document(initial_timestamp)
+            doc_ref = db.collection("ecg_data").document(user_id).collection("readings").document(initial_timestamp)
             doc_ref.set({
             "ecg_data": ecg_data,
             "rr_intervals": rr_intervals.tolist(),
@@ -121,27 +148,31 @@ def upload_data(initial_timestamp,eq_id,ecg_values):
             })
             print("Data uploaded successfully.")
         except Exception as e:
-            print(f"Failed to upload data: {e}")
+            error_message = f"Failed to upload data: {e}"
+            print(error_message)
+            write_to_error_file(error_message, initial_timestamp, eq_id, ecg_values)
             
     
 
 @app.route('/post-data', methods=['POST'])
 def post_data():
     # Handle post request to receive and process ECG data
-    
+
     try:
         data = request.json #Get json data from request
         #Extract parameters
-        initial_timestamp = data.get('initialTimestamp')  
-        eq_id = data.get('eqID') 
-        ecg_values = data.get('ecgValues', [])
+        initial_timestamp = data.get("initialTimestamp")  
+        eq_id = data.get("eqID") 
+        ecg_values = data.get("ecgValues", [])
         
         # upload data to firestore
         upload_data(initial_timestamp,eq_id,ecg_values)
-        
+
         return jsonify({"message": "Data received successfully"}), 200 # Return success code
     except Exception as e:
-        logging.exception("Error: %s", e)
+        error_message = f"Error processing request: {e}"
+        print(error_message)
+        write_to_error_file(error_message, None, None, None)
         return jsonify({"error": "Server error"}), 500 #Return error
 
         
@@ -158,10 +189,10 @@ def start_table_update_thread(interval=3600 * 24): # update once a day
     thread.daemon = True  # Daemon threads as daemon, stops at the end of the program
     thread.start() # Start the thread
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Initial update of the device id
     update_device_user_table()
     # Start the periodic update thread
     start_table_update_thread()
-    app.run(host='0.0.0.0', port=5000, debug=True) # Run the flask app on all available IP adresses on port 5000
+    app.run(host="0.0.0.0", port=5000, debug=True) # Run the flask app on all available IP adresses on port 5000
 
